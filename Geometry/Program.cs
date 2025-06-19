dccc6288de878982e7f9cf90cf87f90dd0335eca
Sky_Time87 <blackshadow1609@yandex.ru>
2025-06-19 16:35:42 +0300

    Geometry

---
+++ Geometry/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Geometry
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			IntPtr hwnd = GetConsoleWindow();
+			Graphics graphics = Graphics.FromHwnd(hwnd);
+			Rectangle window_rect = new Rectangle
+				(
+				Console.WindowLeft, Console.WindowTop,
+				Console.WindowWidth, Console.WindowHeight
+				);
+			PaintEventArgs e = new PaintEventArgs(graphics, window_rect);
+			e.Graphics.DrawRectangle(new Pen(Color.Green, 2), 500, 500, 100, 100);
+
+			//Shape shape = new Shape();	//Невозможно создать экземпляр абстрактного класса;
+			Square square = new Square(10, Color.Red);
+			square.Info();
+		}
+		[DllImport("kernel32.dll")]
+		public static extern IntPtr GetConsoleWindow();
+		[DllImport("kernel32.dll")]
+		public static extern IntPtr GetDC(IntPtr hwnd);
+	}
+}
---
+++ Geometry/Square.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Geometry
+{
+	class Square:Shape
+	{
+		double side;
+		public double Side
+		{
+			get
+			{
+				return side;
+			}
+			set
+			{
+				side = value;
+			}
+		}
+		public Square(double side, Color color) : base(color)
+		{
+			Side = side;
+		}
+		public override double GetArea()
+		{
+			return Side * Side;
+		}
+		public override double GetPerimeter()
+		{
+			return Side * 4;
+		}
+		public override void Draw()
+		{
+			for (int i = 0; i < Side; i++)
+			{
+				for(int j = 0; j < Side; j++)
+				{
+					Console.Write("* ");
+				}
+				Console.WriteLine();
+			}
+		}
+		public override void Info()
+		{
+			Console.WriteLine($"Длина стороны: {Side}");
+			base.Info();
+		}
+	}
+}
