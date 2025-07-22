35e06f539fe20f54acb4fcc78441fbb249a9b447
Sky_Time87 <blackshadow1609@yandex.ru>
2025-07-22 11:21:37 +0300

    Triangle HW DONE

---
+++ Geometry/EquilateralTriangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Geometry
+{
+	class EquilateralTriangle : Triangle
+	{
+		public EquilateralTriangle(int side, int start_x, int start_y, int line_width, Color color)
+			: base(side, (int)(side * Math.Sqrt(3) / 2), start_x, start_y, line_width, color) { }
+
+		public override double GetArea() => (Math.Sqrt(3) / 4) * Base * Base;
+
+		public override double GetPerimeter() => 3 * Base;
+
+		public override void Draw(PaintEventArgs e)
+		{
+			Point[] points = new Point[3];
+			points[0] = new Point(StartX, StartY + Height);
+			points[1] = new Point(StartX + Base, StartY + Height);
+			points[2] = new Point(StartX + Base / 2, StartY);
+
+			e.Graphics.DrawPolygon(new Pen(Color, LineWidth), points);
+		}
+	}
+}
